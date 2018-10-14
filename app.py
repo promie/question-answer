b3914561f5361947270e766cec3006cdb4b82d07
Promie Yutasane <promie.yutasane@gmail.com>
2018-10-14 15:10:45 +1100

    Updated the titles for each pages

--- app.py
+++ app.py
@@ -7,42 +7,42 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', title='Home')
 
 
 @app.route('/answer')
 def answer():
-    return render_template('answer.html')
+    return render_template('answer.html', title='Answer')
 
 
 @app.route('/ask')
 def ask():
-    return render_template('ask.html')
+    return render_template('ask.html', title='Ask')
 
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', title='Login')
 
 
 @app.route('/question')
 def question():
-    return render_template('question.html')
+    return render_template('question.html', title='Question')
 
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('register.html', title='Register')
 
 
 @app.route('/unanswered')
 def unanswered():
-    return render_template('unanswered.html')
+    return render_template('unanswered.html', title="Unanswered")
 
 
 @app.route('/users')
 def users():
-    return render_template('users.html')
+    return render_template('users.html', title='Users')
 
 
 if __name__ == '__main__':
